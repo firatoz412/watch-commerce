191afbdcaab01fefeb7d7c0a40e65d4364a5b56a
Fırat <firatoz412@gmail.com>
2026-03-29 16:04:43 +0300

    cart repository sınıfı gğncellendi

--- commerce/src/main/java/com/watch/commerce/repository/CartRepository.java
+++ commerce/src/main/java/com/watch/commerce/repository/CartRepository.java
@@ -1,12 +1,14 @@
 package com.watch.commerce.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.watch.commerce.model.Cart;
 
 public interface  CartRepository extends JpaRepository<Cart, Long> {
 
-    Cart findByUserId(Long userId);
+    Optional<Cart> findByUserId(Long userId);
 
     void deleteCartByUserId(Long userId);
 
