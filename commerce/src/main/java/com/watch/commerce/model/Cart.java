59ae17f1d52c7bea9a84ed862fb741e7a1ecc05b
Fırat <firatoz412@gmail.com>
2026-03-31 17:33:53 +0300

    cart sınıfında düzenlemeler yapıldı

--- commerce/src/main/java/com/watch/commerce/model/Cart.java
+++ commerce/src/main/java/com/watch/commerce/model/Cart.java
@@ -1,8 +1,6 @@
 package com.watch.commerce.model;
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
@@ -13,8 +11,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
@@ -38,30 +34,17 @@ public class Cart {
     @JoinColumn(name = "user_id")
     private User user;
     
-    //bir sepette bir den fazla kart item olabilir
-    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL)
-    private List<CartItem> cartItems = new ArrayList<>();
-
-    private BigDecimal totalPrice = BigDecimal.ZERO;
-
-    private int quantity;
-
-    @ManyToMany(cascade = CascadeType.ALL)
-    @JoinTable(
-        name = "cart_products",
-        joinColumns = @JoinColumn(name = "cart_id"),
-        inverseJoinColumns = @JoinColumn(name = "product_id")
-    )
-    private List<Product> products = new ArrayList<>();
-
-
     //sepettki itemler hashsette tutulur
     //her bir ürün sete bir kez eklenir wuantity ile miktarı arttırlır
-    @OneToMany(mappedBy="cart",cascade=CascadeType.ALL,orphanRemoval=true)
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL,orphanRemoval=true)
     @JsonManagedReference
     private Set<CartItem> items = new HashSet<>();
 
-    private void updateTotalPrice(){
+    private BigDecimal totalPrice = BigDecimal.ZERO;
+
+
+    //sepet her değiştiğinde toplam fiyat güncellensin
+    public void updateTotalPrice(){
         this.totalPrice = items.stream().map(item ->{
 
          BigDecimal unitPrice = item.getUnitPrice();
@@ -85,6 +68,10 @@ public class Cart {
         this.items.remove(item);
         item.setCart(this);
         updateTotalPrice();
+    }
+
+    public int getTotalItemCount() {
+        return items.stream().mapToInt(CartItem::getQuantity).sum();
     }
 
     
