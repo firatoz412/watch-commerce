331ced05e0601ffedbe28d6e571a47c417ae4850
Fırat <firatoz412@gmail.com>
2026-04-11 17:42:47 +0300

    config,model,repository ve cartHmtl'de düzenlemeler yapıldı

--- commerce/src/main/java/com/watch/commerce/repository/OrderRepository.java
+++ commerce/src/main/java/com/watch/commerce/repository/OrderRepository.java
@@ -1,9 +1,21 @@
 package com.watch.commerce.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.watch.commerce.model.Order;
+import com.watch.commerce.model.User;
+
+
 public interface OrderRepository extends JpaRepository<Order, Long>{
+
+
+    List<Order> findByUserOrderByOrderDateDesc(User user);
+    
+
+
+
 
 
 
--- commerce/src/main/java/com/watch/commerce/service/cart/CartService.java
+++ commerce/src/main/java/com/watch/commerce/service/cart/CartService.java
@@ -74,12 +74,6 @@ public class CartService implements ICartService {
         return cartRepository.save(cart);
     }
 
-    //yok ise user için yeni cart oluştur
-    Cart cart = new Cart();
-    cart.setUser(user);
-    return cartRepository.save(cart);
-}
-
 
     @Override
     public Set<CartItem> getItems(Long cartId) {
