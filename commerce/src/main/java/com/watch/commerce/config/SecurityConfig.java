d2ede263a1b8797314d8b7598ffd4372c845fe6e
Fırat <firatoz412@gmail.com>
2026-05-11 21:09:00 +0300

    eksik sınıflar gönderildi......

---
+++ commerce/src/main/java/com/watch/commerce/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.watch.commerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/", "/login", "/register", "/css/**", "/js/**", "/images/**","/products/**","/auth/**").permitAll()
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .requestMatchers("/hesabim/**", "/cart/**", "/order/**", "/payment/**").authenticated()
+                .anyRequest().authenticated()
+            )
+            .formLogin(form -> form
+                .loginPage("/login")
+                .successHandler((request,response,authentication)->{
+                    boolean isAdmin = authentication.getAuthorities().stream()
+                    .anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN")); 
+                    if(isAdmin){
+                        response.sendRedirect("/admin/dashboard");
+                    }else{
+                        response.sendRedirect("/");
+                    }
+                })
+                .permitAll()
+            )
+            .logout(logout -> logout
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")//çıkış yapan kullanıcı / 'e gider
+                .invalidateHttpSession(true)//kullanıcı oturumunu siler
+                .deleteCookies("JSESSIONID")
+                .permitAll()
+            );
+
+        return http.build();
+    }
+
+
+
+}
